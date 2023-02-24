e1fbed48fdd5ff05e7fef75e6fb96cd07d0fa7cc
Datsunco <danya200375d@gmail.com>
2023-02-24 21:19:03 +0300

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import './Form.css'
 import {useTelegram} from "../../hooks/useTelegram";
+import {useCallback} from "react";
 
 const Form = () => {
     const [city, setCity] = useState('');
