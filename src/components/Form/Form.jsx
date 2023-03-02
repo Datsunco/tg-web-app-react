48ed18e7a39e3ddba6d06425e21c73f8fbc6635f
Datsunco <danya200375d@gmail.com>
2023-03-02 11:32:43 +0300

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -30,7 +30,7 @@ const Form = () => {
         return () => {
             tg.offEvent('mainButtonClicked', onSendData)
         }
-    },[])
+    },[onSendData()])
 
     useEffect(() => {
         tg.MainButton.setParams({
